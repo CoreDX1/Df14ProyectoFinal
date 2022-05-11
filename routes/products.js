3f79e7181dbac3d0f1318e7e0926e8f0319c6dae
CoreDX1 <chrismaquis@gmail.com>
2022-05-11 09:57:53 -0300

    WriteFile

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,36 @@
 const express = require('express')
+const fs = require('fs')
 const {Router} = express
 const router = new Router()
 
 
 
 router.get('/', (req, res) => {
-    res.render('./products/index')
+    try{
+        const data = fs.readFileSync(__dirname + '/../products.json', 'utf8')
+        res.render('./products/index.ejs', {data:JSON.parse(data)})
+    }catch(err) {
+        console.log('Error al leer')
+    }
+})
+
+router.post('/', (req, res) => {
+    try{
+        const data = fs.readFileSync(__dirname + '/../products.json', 'utf8')
+        let json = JSON.parse(data)
+        let oj = req.body
+        json.push(oj)
+        try{
+            const write = fs.writeFileSync(__dirname + '/../products.json', JSON.stringify(json, null, 2), 'utf8')
+            console.log(json)
+            return write
+        }catch(err){
+            console.log('Error al Escribir')
+        }
+        res.send('Se Guardo')
+    }catch(err) {
+        console.log('Error al leer')
+    }
 })
 
 module.exports = router
